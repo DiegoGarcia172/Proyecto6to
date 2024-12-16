5c18a9c72fe55aaf3004dc583ac5162513e93781
DiegoGarcia172 <89672183+DiegoGarcia172@users.noreply.github.com>
2024-12-15 23:35:17 -0600

    yml

--- Lprocesar.php
+++ Lprocesar.php
@@ -28,7 +28,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
         exit();
     } else {
-        echo "Correo o contraseña incorrectos.";
+        echo "Correo o contraseña malos.";
     }
 }
 ?>
